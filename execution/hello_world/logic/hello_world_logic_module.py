e82bde04c62d0a9637d918386571a3d3b187810d
dtoth <dtoth@users.noreply.github.com>
2025-04-01 14:10:42 -0400

    Clean up old files after merging refactor-kt-package

--- execution/hello_world/logic/hello_world_logic_module.py
+++
@@ -1,4 +0,0 @@
-def hello_world_prepender(s: str) -> str:
-    """Prepend 'Hello World' to each line in the input string"""
-
-    return "\n".join(["Hello World: " + line for line in s.split("\n")])
